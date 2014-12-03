f7f208574c73e3f1f6bebc952ab776551c5ea5f2
fishdrummer <mathieukorkos@gmail.com>
2014-12-03 01:06:52 +0100

    Profile implemented

---
+++ Stouring/src/com/junior/stouring/HomeActivity.java
@@ -0,0 +1,80 @@
+package com.junior.stouring;
+
+import com.junior.stouring.drawer.DrawerActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+public class HomeActivity extends DrawerActivity {
+
+	@Override
+	protected void onCreate(Bundle arg0) {
+		
+		super.onCreate(arg0);
+		setContentView(R.layout.activity_home);
+		
+		
+		
+		Typeface latoLight = Typeface.createFromAsset(getAssets(), "Lato-Light.ttf");
+		
+		Button buttonMaVille = (Button) findViewById(R.id.buttonmaville);
+		buttonMaVille.setText("Ma ville");
+		buttonMaVille.setTypeface(latoLight);
+		
+		Button buttonDecouvrir = (Button) findViewById(R.id.buttondecouvrir);
+		buttonDecouvrir.setText("Découvrir");
+		buttonDecouvrir.setTypeface(latoLight);
+		buttonDecouvrir.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+			
+				Intent launchDecouvrir = new Intent(HomeActivity.this,
+						TouringListActivity.class);
+				startActivity(launchDecouvrir);
+			}
+		});
+		
+		Button buttonAchats = (Button) findViewById(R.id.buttonachats);
+		buttonAchats.setText("Achats");
+		buttonAchats.setTypeface(latoLight);
+		
+		Button buttonProfil = (Button) findViewById(R.id.buttonprofil);
+		buttonProfil.setText("Mon Profil");
+		buttonProfil.setTypeface(latoLight);
+		buttonProfil.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+			
+				Intent launchProfile = new Intent(HomeActivity.this,
+						ProfileActivity.class);
+				startActivity(launchProfile);
+			}
+		});
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.home, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
---
+++ Stouring/src/com/junior/stouring/ProfileActivity.java
@@ -0,0 +1,129 @@
+package com.junior.stouring;
+
+import com.junior.stouring.drawer.DrawerActivity;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class ProfileActivity extends DrawerActivity {
+
+	public static final String PREFS_NAME = "UserPrefs";
+
+	@Override
+	protected void onCreate(Bundle arg0) {
+		super.onCreate(arg0);
+		setContentView(R.layout.activity_profile);
+
+		// Restore preferences
+		final SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+		
+
+		final TextView addressContent = (TextView) findViewById(R.id.addressContent);
+		addressContent.setText(settings.getString("addressContent", "aucune adresse"));
+
+		final TextView name = (TextView) findViewById(R.id.name);
+		name.setText(settings.getString("name", "John Doe"));
+
+
+
+
+		addressContent.setOnClickListener(new View.OnClickListener(){
+
+			@Override
+			public void onClick(View arg0) {
+				showAlert("Adresse", addressContent, "addressContent", settings);
+				
+			}
+
+
+		});
+
+
+
+		name.setOnClickListener(new View.OnClickListener(){
+
+			@Override
+			public void onClick(View arg0) {
+				showAlert("Prénom - Nom", name, "name",settings);
+			}
+
+
+		});
+		
+		
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.profile, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+
+	public void showAlert(String title, final TextView container, final String containerStr, SharedPreferences settings){
+		
+		String content = container.getText().toString();
+		
+		final SharedPreferences.Editor editor = settings.edit();
+
+		AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+				ProfileActivity.this);
+
+		// set title
+		alertDialogBuilder.setTitle(title);
+
+		// Set an EditText view to get user input 
+		final EditText input = new EditText(ProfileActivity.this);
+		input.setText(content);
+		alertDialogBuilder.setView(input);
+
+		// set dialog message
+		alertDialogBuilder
+		.setCancelable(false)
+		.setPositiveButton("OK",new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialog,int id) {
+				String newTxt = input.getText().toString();
+				container.setText(newTxt);
+				editor.putString(containerStr, newTxt);
+				editor.commit();
+
+			}
+		})
+		.setNegativeButton("Annuler",new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialog,int id) {
+				// if this button is clicked, just close
+				// the dialog box and do nothing
+				dialog.cancel();
+			}
+		});
+
+		// create alert dialog
+		AlertDialog alertDialog = alertDialogBuilder.create();
+
+		// show it
+		alertDialog.show();
+
+	}
+
+}
